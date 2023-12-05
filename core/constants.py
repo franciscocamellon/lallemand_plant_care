18e11453e283194a311dd18dbb89f1ddd37be469
franciscocamellon <franciscocamellon@gmail.com>
2023-12-04 21:08:24 -0300

    Add constants

---
+++ core/constants.py
@@ -0,0 +1,4 @@
+QGIS_TOC_GROUPS = ['Raw_Data', 'Reprojected_Data', 'Sampling', 'Kriging', 'Validation', 'Error_Compensation',
+                   'Gain_Surface']
+DIRECTORY_STRUCTURE = ['00_Data', '01_Kriging', '02_Validation', '03_Error_Compensation', '04_Gain_Surface',
+                       '05_Results']
