61a5dbf4277f291e710b0283fb19c4cc510e82df
franciscocamellon <franciscocamellon@gmail.com>
2023-12-08 23:52:24 -0300

    Add new logic to handle CRUD

--- core/services/message_service.py
+++ core/services/message_service.py
@@ -23,13 +23,15 @@
 """
 from qgis.core import Qgis
 from qgis.gui import QgsMessageBar
+from qgis.PyQt.QtWidgets import QMessageBox
+from qgis.PyQt.QtCore import QCoreApplication
 
 
 class MessageService:
-    def __init__(self, iface):
-        self.iface = iface
+    def __init__(self):
+        pass
 
-    def show_message(self, message, message_type='info'):
+    def show_message(self, iface, message, message_type='info'):
         message_level = 0
 
         if message_type == 'error':
@@ -39,4 +41,43 @@ class MessageService:
         elif message_type == 'success':
             message_level = 3
 
-        self.iface.messageBar().pushMessage(message_type, message, level=message_level, duration=5)
+        iface.messageBar().pushMessage(message_type, message, level=message_level, duration=5)
+
+    @staticmethod
+    def setIconType(iconType):
+        if iconType == 1:
+            return QMessageBox.NoIcon
+        elif iconType == 2:
+            return QMessageBox.Question
+        elif iconType == 3:
+            return QMessageBox.Information
+        elif iconType == 4:
+            return QMessageBox.Warning
+        else:
+            return QMessageBox.Critical
+
+    @staticmethod
+    def setButtonType(buttonType):
+        if buttonType == 1:
+            return QMessageBox.Ok
+        elif buttonType == 2:
+            return QMessageBox.Cancel
+        elif buttonType == 3:
+            return QMessageBox.Close
+        elif buttonType == 4:
+            return QMessageBox.Save
+
+    def messageBox(self, title, message, iconType, buttonType):
+        messageBox = QMessageBox()
+        messageBox.setWindowTitle(self.tr(title))
+        messageBox.setIcon(self.setIconType(iconType))
+        messageBox.setText(self.tr(message))
+        messageBox.setStandardButtons(self.setButtonType(buttonType))
+        messageBox.setDefaultButton(self.setButtonType(buttonType))
+        messageBox.exec_()
+
+    def tr(self, string):
+        """
+        Returns a translatable string with the self.tr() function.
+        """
+        return QCoreApplication.translate('MessageService', string)
